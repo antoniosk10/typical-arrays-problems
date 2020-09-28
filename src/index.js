b38f7dcf3da934f48808e9846ca8959298910182
Your Name <you@example.com>
2020-09-28 23:21:57 +0300

    feat:add solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array = 0) {
+    if (array === 0 || !array.length) {
+        return 0;
+    } else {
+        let min = array[0];
+        array.forEach((el) => {
+            if (el < min) min = el;
+        });
+        return min;
+    }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = 0) {
+    if (array === 0 || !array.length) {
+        return 0;
+    } else {
+        let max = array[0];
+        array.forEach((el) => {
+            if (el > max) max = el;
+        });
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = 0) {
+    if (array === 0 || !array.length) {
+        return 0;
+    } else {
+        let summ = 0;
+        array.forEach((el) => {
+            summ += el;
+        });
+        return summ / array.length;
+    }
 }
